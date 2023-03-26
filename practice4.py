e70fb2f43043a4f1e7ecb9281238f4da70fafce5
sk12304 <127031773+sk12304@users.noreply.github.com>
2023-03-26 18:30:36 +0900

    Add files via upload

---
+++ practice4.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests as req
+
+
+target url = 'https://opentutorials.org/course/1'
+
+
+res = reg.get(target _url)
+soup = BeautifulSoup(res.text, 'html.parser')
+
+
+li_comments = soup.find_all(class_='comment_content')
+li_comments_cleaned = [e.text.strip() for e in li_ comments]
+li_name = soup. select( 'div.name.time > strong')
+li_name_cleaned = [e.text for e in li_name]
+li_time = soup.select( 'div.name.time > a > time')
+li_time_cleaned = [e.text for e in li_time]
+
+
+container = {'name' :li_name_ cleaned, 'time':li_time_cleaned, 'coments': li_comments_cleaned}
+df_table = pd.DataFrame(container)
+df_table
