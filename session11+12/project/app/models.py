fcc88267d706a45234a0a1554cdda36ddb1cb5bf
sk12304 <127031773+sk12304@users.noreply.github.com>
2023-05-01 11:46:51 +0900

    Add files via upload

---
+++ session11+12/project/app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+   title = models.CharField(max_length=50)
+   content = models.TextField()
+   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", default=1)
+
+
+
+   def __str__(self):
+       return self.title
+   
+class Comment(models.Model):
+   post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+   content = models.TextField()
+   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments", default=1)
+
+
+   def __str__(self):
+       return self.content
